b3ed1ab575fa70889f02f01486b9fc93337968df
oshadaov <oshadaov@gmail.com>
2025-02-03 09:08:22 +0530

    modify bookValidation.ts

--- src/validations/bookValidation.ts
+++ src/validations/bookValidation.ts
@@ -1,17 +1,17 @@
 import { body, param, validationResult } from 'express-validator';
 import { Request, Response, NextFunction } from "express";
 
-export const validateBookId = [
+const validateBookId = [
     param('id').isInt().withMessage('ID must be an integer')
 ];
 
-export const validateBook = [
+const validateBook = [
     body('name').notEmpty().withMessage('Name is required'),
     body('author').notEmpty().withMessage('Author is required'),
     body('publishedYear').isInt({ min: 1000, max: 9999 }).withMessage('Valid year required')
 ];
 
-export const validateBookUpdate = [
+const validateBookUpdate = [
     param('id').isInt().withMessage('ID must be an integer'),
     body('name').notEmpty().withMessage('Name is required'),
     body('author').notEmpty().withMessage('Author is required'),
@@ -26,11 +26,5 @@ export const validateBookUpdate = [
 ];
 
 
-// Separate function to handle validation results
-export const handleValidationErrors = (req: Request, res: Response, next: NextFunction) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        return res.status(400).json({  message: "All fields are required"  });
-    }
-    next();
-};
+
+export {validateBookId, validateBook ,validateBookUpdate}
